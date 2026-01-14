d209410da7ba87bb32a64b1abe16950f10aecd3e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-01-13 18:30:42 -0800

    add github stars

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,10 +4,14 @@ import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { pluginReadmeSync } from "./scripts/plugin-readme-sync";
+import { githubStarsPlugin } from "./src/ssg/github-stars-plugin";
 
 const config = defineConfig({
   plugins: [
     pluginReadmeSync(),
+    githubStarsPlugin({
+      token: process.env.LIX_WEBSITE_GITHUB_TOKEN,
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
