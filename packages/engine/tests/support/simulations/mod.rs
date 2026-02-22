793f7613c9b17d1e3e679ac953177d248aef595f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-02-21 17:58:18 -0800

    engine: add timestamp shuffle functionality and corresponding tests

--- packages/engine/tests/support/simulations/mod.rs
+++ packages/engine/tests/support/simulations/mod.rs
@@ -1,10 +1,12 @@
 mod materialization;
 mod postgres;
 mod sqlite;
+mod timestamp_shuffle;
 
 pub use materialization::materialization_simulation;
 pub use postgres::postgres_simulation;
 pub use sqlite::sqlite_simulation;
+pub use timestamp_shuffle::timestamp_shuffle_simulation;
 
 use crate::support::simulation_test::Simulation;
 
@@ -13,5 +15,6 @@ pub fn default_simulations() -> Vec<Simulation> {
         sqlite_simulation(),
         postgres_simulation(),
         materialization_simulation(),
+        timestamp_shuffle_simulation(),
     ]
 }
