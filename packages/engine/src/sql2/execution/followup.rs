7dfdbbfd7075d17e90a9facaa9ab00dc33a303d2
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-02-23 17:52:24 -0800

    refactor(engine/sql2): bypass sql bridge for followup execution

--- packages/engine/src/sql2/execution/followup.rs
+++ packages/engine/src/sql2/execution/followup.rs
@@ -1,3 +1,3 @@
-pub(crate) use super::super::super::sql_bridge_runtime::{
+pub(crate) use super::super::super::sql_followup_runtime::{
     build_delete_followup_statements, build_update_followup_statements,
 };
