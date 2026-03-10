909dde0cae2058ae2b5edbdfbb19163c2e068933
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-03-10 15:18:19 -0700

    Collapse internal state postprocess execution

--- packages/engine/src/sql/execution/run.rs
+++ packages/engine/src/sql/execution/run.rs
@@ -4,19 +4,13 @@ use crate::deterministic_mode::DeterministicSettings;
 use crate::deterministic_mode::RuntimeFunctionProvider;
 use crate::functions::SharedFunctionProvider;
 use crate::schema_registry::register_schema_sql_statements;
-use crate::state_commit_stream::{
-    state_commit_stream_changes_from_postprocess_rows, StateCommitStreamChange,
-    StateCommitStreamOperation,
-};
+use crate::state_commit_stream::StateCommitStreamChange;
 use crate::{Engine, LixError, LixTransaction, QueryResult};
 
 use super::super::contracts::effects::PlanEffects;
 use super::super::contracts::execution_plan::ExecutionPlan;
 use super::super::contracts::executor_error::ExecutorError;
-use crate::internal_state::followup::{
-    build_delete_followup_statements, build_update_followup_statements,
-};
-use crate::internal_state::PostprocessPlan;
+use crate::internal_state::followup::execute_postprocess_with_transaction;
 use super::super::contracts::result_contract::ResultContract;
 use super::super::planning::lower_sql::lower_to_prepared_statements;
 use super::execute_prepared::{execute_prepared_with_backend, execute_prepared_with_transaction};
@@ -61,131 +55,27 @@ pub(crate) async fn execute_plan_sql(
                 .begin_transaction()
                 .await
                 .map_err(ExecutorError::execute)?;
-            let result =
-                match execute_prepared_with_transaction(transaction.as_mut(), &prepared_statements)
-                    .await
-                {
-                    Ok(result) => result,
-                    Err(error) => {
-                        let _ = transaction.rollback().await;
-                        return Err(ExecutorError::execute(error));
-                    }
-                };
-
-            match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) if should_refresh_file_cache => {
-                    match super::super::super::collect_postprocess_file_cache_targets(
-                        &result.rows,
-                        &update_plan.schema_key,
-                    ) {
-                        Ok(targets) => {
-                            postprocess_file_cache_targets.extend(targets);
-                        }
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
-                }
-                PostprocessPlan::VtableDelete(delete_plan) if should_refresh_file_cache => {
-                    match super::super::super::collect_postprocess_file_cache_targets(
-                        &result.rows,
-                        &delete_plan.schema_key,
-                    ) {
-                        Ok(targets) => {
-                            postprocess_file_cache_targets.extend(targets);
-                        }
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
-                }
-                _ => {}
-            }
-
-            match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) => {
-                    match state_commit_stream_changes_from_postprocess_rows(
-                        &result.rows,
-                        &update_plan.schema_key,
-                        StateCommitStreamOperation::Update,
-                        writer_key,
-                    ) {
-                        Ok(changes) => state_commit_stream_changes.extend(changes),
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
-                }
-                PostprocessPlan::VtableDelete(delete_plan) => {
-                    match state_commit_stream_changes_from_postprocess_rows(
-                        &result.rows,
-                        &delete_plan.schema_key,
-                        StateCommitStreamOperation::Delete,
-                        writer_key,
-                    ) {
-                        Ok(changes) => state_commit_stream_changes.extend(changes),
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
-                }
-            }
-
-            let mut followup_functions = functions.clone();
-            let followup_params = prepared_statements
-                .first()
-                .map(|statement| statement.params.as_slice())
-                .unwrap_or(&[]);
-            let followup_statements = match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) => {
-                    match build_update_followup_statements(
-                        transaction.as_mut(),
-                        update_plan,
-                        &result.rows,
-                        writer_key,
-                        &mut followup_functions,
-                    )
-                    .await
-                    {
-                        Ok(statements) => statements,
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
-                }
-                PostprocessPlan::VtableDelete(delete_plan) => {
-                    match build_delete_followup_statements(
-                        transaction.as_mut(),
-                        delete_plan,
-                        &result.rows,
-                        followup_params,
-                        writer_key,
-                        &mut followup_functions,
-                    )
-                    .await
-                    {
-                        Ok(statements) => statements,
-                        Err(error) => {
-                            let _ = transaction.rollback().await;
-                            return Err(ExecutorError::execute(error));
-                        }
-                    }
+            let outcome = match execute_postprocess_with_transaction(
+                transaction.as_mut(),
+                &prepared_statements,
+                postprocess_plan,
+                should_refresh_file_cache,
+                functions,
+                writer_key,
+            )
+            .await
+            {
+                Ok(outcome) => outcome,
+                Err(error) => {
+                    let _ = transaction.rollback().await;
+                    return Err(ExecutorError::execute(error));
                 }
             };
-            if let Err(error) =
-                execute_prepared_with_transaction(transaction.as_mut(), &followup_statements).await
-            {
-                let _ = transaction.rollback().await;
-                return Err(ExecutorError::execute(error));
-            }
             transaction.commit().await.map_err(ExecutorError::execute)?;
             plugin_changes_committed = true;
-            result
+            postprocess_file_cache_targets = outcome.postprocess_file_cache_targets;
+            state_commit_stream_changes = outcome.state_commit_stream_changes;
+            outcome.internal_result
         }
     };
     let public_result = public_result_from_contract(plan.result_contract, &internal_result);
@@ -231,84 +121,20 @@ pub(crate) async fn execute_plan_sql_with_transaction(
             result
         }
         Some(postprocess_plan) => {
-            let result = execute_prepared_with_transaction(transaction, &prepared_statements)
-                .await
+            let outcome = execute_postprocess_with_transaction(
+                transaction,
+                &prepared_statements,
+                postprocess_plan,
+                should_refresh_file_cache,
+                functions,
+                writer_key,
+            )
+            .await
                 .map_err(ExecutorError::execute)?;
-
-            match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) if should_refresh_file_cache => {
-                    let targets = super::super::super::collect_postprocess_file_cache_targets(
-                        &result.rows,
-                        &update_plan.schema_key,
-                    )
-                    .map_err(ExecutorError::execute)?;
-                    postprocess_file_cache_targets.extend(targets);
-                }
-                PostprocessPlan::VtableDelete(delete_plan) if should_refresh_file_cache => {
-                    let targets = super::super::super::collect_postprocess_file_cache_targets(
-                        &result.rows,
-                        &delete_plan.schema_key,
-                    )
-                    .map_err(ExecutorError::execute)?;
-                    postprocess_file_cache_targets.extend(targets);
-                }
-                _ => {}
-            }
-
-            match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) => {
-                    let changes = state_commit_stream_changes_from_postprocess_rows(
-                        &result.rows,
-                        &update_plan.schema_key,
-                        StateCommitStreamOperation::Update,
-                        writer_key,
-                    )
-                    .map_err(ExecutorError::execute)?;
-                    state_commit_stream_changes.extend(changes);
-                }
-                PostprocessPlan::VtableDelete(delete_plan) => {
-                    let changes = state_commit_stream_changes_from_postprocess_rows(
-                        &result.rows,
-                        &delete_plan.schema_key,
-                        StateCommitStreamOperation::Delete,
-                        writer_key,
-                    )
-                    .map_err(ExecutorError::execute)?;
-                    state_commit_stream_changes.extend(changes);
-                }
-            }
-
-            let mut followup_functions = functions.clone();
-            let followup_params = prepared_statements
-                .first()
-                .map(|statement| statement.params.as_slice())
-                .unwrap_or(&[]);
-            let followup_statements = match postprocess_plan {
-                PostprocessPlan::VtableUpdate(update_plan) => build_update_followup_statements(
-                    transaction,
-                    update_plan,
-                    &result.rows,
-                    writer_key,
-                    &mut followup_functions,
-                )
-                .await
-                .map_err(ExecutorError::execute)?,
-                PostprocessPlan::VtableDelete(delete_plan) => build_delete_followup_statements(
-                    transaction,
-                    delete_plan,
-                    &result.rows,
-                    followup_params,
-                    writer_key,
-                    &mut followup_functions,
-                )
-                .await
-                .map_err(ExecutorError::execute)?,
-            };
-            execute_prepared_with_transaction(transaction, &followup_statements)
-                .await
-                .map_err(ExecutorError::execute)?;
+            postprocess_file_cache_targets = outcome.postprocess_file_cache_targets;
+            state_commit_stream_changes = outcome.state_commit_stream_changes;
             plugin_changes_committed = true;
-            result
+            outcome.internal_result
         }
     };
     let public_result = public_result_from_contract(plan.result_contract, &internal_result);
