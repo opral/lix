f15277fa9a5d865507307808de724ae388fc1d6e
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2026-07-27 20:28:18 +0000

    perf: use mimalloc in native entrypoints

--- packages/js-sdk/native/lib.rs
+++ packages/js-sdk/native/lib.rs
@@ -1,4 +1,8 @@
 #[cfg(not(target_family = "wasm"))]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(not(target_family = "wasm"))]
 mod napi;
 mod telemetry;
 #[cfg(target_family = "wasm")]
